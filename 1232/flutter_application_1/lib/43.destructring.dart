aac965366e756db33db18f8d47141a309f0ce168
yang <163013769+Eunsseob@users.noreply.github.com>
2025-12-26 14:05:18 +0900

    feat. 25.12.26

---
+++ 1232/flutter_application_1/lib/43.destructring.dart
@@ -0,0 +1,21 @@
+
+void main() {
+
+  final [x1, y1, ..rest1, z1] = numbers;
+  print(x1);
+  print(y1);
+  print(z1);
+  print(rest1);
+  print('================');
+
+  // _ 는 건너 뛰기
+  final [x2, _, ...rest2, z2, _] == nubers;
+  print(x2);
+  print(y2);
+  print(z2);
+  print(rest2);
+  print('================');
+
+  // Map
+  final minJiMap = {'name' : '민지', 'age' : 20};
+}
